f47f8ea163dc2f2f355c28b705f28cec9ac2bda5
kurt <kurt@kurt-ThinkPad-T440>
2017-04-27 12:37:28 -0400

    search function returns node, find function is human readable

--- Homework6/src/binarySearchTree/BST.java
+++ Homework6/src/binarySearchTree/BST.java
@@ -8,10 +8,16 @@ public class BST {
 		root = null;
 	}
 
+	/**
+	 * Checks if tree is empty
+	 * 
+	 * @return boolean
+	 */
 	public boolean isEmpty() {
 		return this.root == null;
 	}
 
+	// Getters and Setters
 	public Node getRoot() {
 		return root;
 	}
@@ -22,8 +28,13 @@ public class BST {
 
 	}
 
+	/**
+	 * Insert method adds string to tree
+	 * 
+	 * @param key
+	 */
 	public void insert(String key) {
-
+		// If tree is empty make root otherwise recursively call insert
 		if (isEmpty()) {
 
 			setRoot(key);
@@ -34,10 +45,18 @@ public class BST {
 
 	}
 
+	/**
+	 * Inserts new node at correct position in tree
+	 * 
+	 * @param p
+	 * @param data
+	 */
 	public void insert(Node p, String data) {
-
+		// Creates new node with input data
 		Node newNode = new Node(data);
-
+		// If the string comes before the parent it's placed to the left if left
+		// node is free, if left node is full recursively calls insert with left
+		// node as parent
 		if (p.getData().compareToIgnoreCase(data) > 0) {
 
 			if (p.getLeft() != null) {
@@ -49,7 +68,9 @@ public class BST {
 			}
 
 		}
-
+		// If the string comes after the parent it's placed to the right if the
+		// right node is free, if right node is full recursively calls insert
+		// with right node as parent
 		else if (p.getData().compareToIgnoreCase(data) < 0) {
 
 			if (p.getRight() != null) {
@@ -61,7 +82,7 @@ public class BST {
 			}
 
 		}
-
+		// If the string matches the parent then the frequency is incrimented
 		else if (p.getData().equalsIgnoreCase(data)) {
 
 			p.incFreq();
@@ -70,22 +91,32 @@ public class BST {
 
 	}
 
+	/**
+	 * Prints tree in order
+	 */
 	public void inOrder() {
 		inOrder(root);
 	}
 
+	/**
+	 * Prints tree in order
+	 * 
+	 * @param curr
+	 */
 	public void inOrder(Node curr) {
-
+		// If current node is not null check if it has a left or right child
 		if (curr != null) {
-
+			// If current node has left child recursively call inOrder method
+			// starting at left node
 			if (curr.getLeft() != null) {
 
 				inOrder(curr.getLeft());
 
 			}
-
-			System.out.print(curr.getData() + " ");
-
+			// If left node is null print current node's data
+			System.out.println(curr.getData() + " " + curr.getFreq());
+			// If current node has a right child recursively call inOrder method
+			// starting at right node
 			if (curr.getRight() != null) {
 
 				inOrder(curr.getRight());
@@ -95,5 +126,63 @@ public class BST {
 		}
 
 	}
+
+	public void find(String find) {
+
+		if (isEmpty()) {
+			System.out.println("Tree is Empty!");
+		} else {
+			find(root, find);
+		}
+
+	}
+
+	public void find(Node curr, String find) {
+
+		if (curr.getData().equals(find)) {
+			System.out.println(find + " is in tree!");
+
+		}
+
+		if (curr.getData().compareToIgnoreCase(find) > 0) {
+
+			if (curr.getLeft() != null) {
+				find(curr.getLeft(), find);
+			} else {
+				System.out.println("Item " + find + " not found!");
+			}
+
+		}
+
+		if (curr.getData().compareToIgnoreCase(find) < 0) {
+			if (curr.getRight() != null) {
+
+				find(curr.getRight(), find);
+
+			} else {
+				System.out.println("Item " + find + " not found!");
+			}
+
+		}
+
+	}
+	
+	public Node search(String find){
+		Node current = root;
+		if(isEmpty())
+			return null;
+		while(current != null){
+			if(current.getData().equals(find)){
+				return current;
+			}
+			else if(current.getData().compareToIgnoreCase(find)>0){
+				current = current.getLeft();
+			}else{
+				current = current.getRight();
+			}
+		}
+		return null;
+	}
+	
 
 }
--- Homework6/src/binarySearchTree/Main.java
+++ Homework6/src/binarySearchTree/Main.java
@@ -5,18 +5,21 @@ public class Main {
 	public static void main(String[]args){
 		
 		BST test = new BST();
-		String testA = "a", testB = "b", testB1 = "b";
+		String testA = "a", testB = "b", testB1 = "b", c = "c", d = "d";
 		
-		
+		test.insert(c);
 		test.insert(testA);
 		test.insert(testB);
 		test.insert(testB1);
+		test.insert(c);
+		test.insert(d);
 		
 		
-		System.out.println(test.getRoot().getRight().getData());
-		System.out.println(test.getRoot().getRight().getFreq());
 		
+		test.find("f");
 		test.inOrder();
+		
+		System.out.println(test.search("f"));
 		
 	}
 
