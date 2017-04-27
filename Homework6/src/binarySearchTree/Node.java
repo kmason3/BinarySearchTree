abf9cddbbfdc476e494c718ac496c787f51d7921
kurt <kurt@kurt-ThinkPad-T440>
2017-04-27 10:32:44 -0400

    node constructor

--- Homework6/src/binarySearchTree/Node.java
+++ Homework6/src/binarySearchTree/Node.java
@@ -1,5 +1,51 @@
 package binarySearchTree;
 
 public class Node {
+	
+	private Node left, right;
+	private String data;
+	private int freq;
+	
+	public Node(String word, int count){
+		
+		this.setData(word);
+		this.setFreq(1);
+		this.setLeft(null);
+		this.setRight(null);
+		
+	}
+
+	public Node getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public int getFreq() {
+		return freq;
+	}
+
+	public void setFreq(int freq) {
+		this.freq = freq;
+	}
+	
 
 }
