63b24cc428d92855bb1d2e8f41e3fed025cd9064
kurt <kurt@kurt-ThinkPad-T440>
2017-04-27 10:50:05 -0400

    insert method

--- Homework6/src/binarySearchTree/BST.java
+++ Homework6/src/binarySearchTree/BST.java
@@ -7,15 +7,65 @@ public class BST {
 	public BST() {
 		root = null;
 	}
+	
+	public boolean isEmpty(){
+		return root == null;
+	}
 
 	public Node getRoot() {
 		return root;
 	}
 
-	public void setRoot(Node root) {
+	public void setRoot(String word) {
 
-		this.root = root;
+		this.root = new Node(word);
 
+	}
+	
+	public void insert(String key){
+		
+		insert(root, key);
+		
+	}
+	
+	public void insert(Node p, String data){
+		
+		if(isEmpty()){
+			
+			setRoot(data);
+			
+		}
+		
+		if(p.getData().compareToIgnoreCase(data) < 0){
+			
+			if(p.getLeft() != null){
+				
+				insert(p.getLeft(), data);
+				
+			}else{
+				p.setLeft(data);
+			}
+			
+		}
+		
+		if(p.getData().compareToIgnoreCase(data) > 0){
+			
+			if(p.getRight() != null){
+				
+				insert(p.getRight(), data);
+				
+			}else{
+				p.setRight(data);
+			}
+			
+		}
+		
+		if(p.getData().equalsIgnoreCase(data)){
+			
+			p.incFreq();
+			
+		}
+		
 	}
 
 }
