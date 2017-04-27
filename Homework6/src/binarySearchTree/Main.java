97e43ea008d545ae22954950dc4978f27299dd92
kurt <kurt@kurt-ThinkPad-T440>
2017-04-27 11:11:46 -0400

    tests if freq increses with same string entry

--- Homework6/src/binarySearchTree/Main.java
+++ Homework6/src/binarySearchTree/Main.java
@@ -5,14 +5,16 @@ public class Main {
 	public static void main(String[]args){
 		
 		BST test = new BST();
-		String testA = "a", testB = "b";
+		String testA = "a", testB = "b", testB1 = "b";
 		
 		
 		test.insert(testA);
 		test.insert(testB);
+		test.insert(testB1);
+		
 		
 		System.out.println(test.getRoot().getRight().getData());
-		
+		System.out.println(test.getRoot().getRight().getFreq());
 		
 	}
 
