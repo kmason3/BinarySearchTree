e4b9b07cd48ad8c9f8b6ef4a27c74ed0794d5326
kurt <kurt@kurt-ThinkPad-T440>
2017-05-06 15:28:14 -0400

    Impliments file reader and tests methods

--- Homework6/src/binarySearchTree/Main.java
+++ Homework6/src/binarySearchTree/Main.java
@@ -1,43 +1,38 @@
 package binarySearchTree;
 
-public class Main {
-	
-	public static void main(String[]args){
-		
-		BST test = new BST();
-		String testA = "a", testB = "b", testB1 = "b", c = "c", d = "d", h = "h";
-		
-		test.insert(d);
-		
-		test.insert(testB);
-		test.insert(testA);
-		test.insert("b");
-		test.insert(c);
-		test.insert("f");
-		test.insert("g");
-		test.insert("e");
-		test.insert(h);
-		
-	
-//		test.deleteInst("b");
-//		test.deleteInst("d");
-//		test.delete("h");
-//		test.delete("b");
-//		test.delete("a");
-//		test.delete("f");
-//		System.out.println("Root is " + test.getRoot().getData());
-//		System.out.println("Root's left child " + test.getRoot().getLeft().getData());
-//		System.out.println("Root's right child " + test.getRoot().getRight().getData());
-//		System.out.println("b's left child is " + test.search("b").getLeft().getData());
-//		System.out.println("a's right child is " + test.search("a").getRight().getData());
-//		System.out.println("f's left child is " + test.search("f").getLeft().getData());
-//		System.out.println("f's right child is " + test.search("f").getRight().getData());
-//		System.out.println("g's left child is " + test.search("g").getLeft().getData());
-//		System.out.println("g's right child is " + test.search("g").getRight().getData());
-		test.inOrder();
+import java.io.*;
+import java.io.FileReader;
 
-		
-		
+public class Main {
+
+	public static void main(String[] args) {
+
+		BST test = new BST();
+		String filename = "data.txt";
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(filename));
+			String key[] = reader.readLine().split(" ");
+
+			for (int i = 0; i < key.length; i++) {
+				test.insert(key[i]);
+
+			}
+
+			test.inOrder();
+			System.out.println("--Delete 'am' Test:");
+			test.delete("am");
+			test.inOrder();
+			System.out.println("--Delete Instance of 'testing' Test:");
+			test.deleteInst("testing");
+			reader.close();
+			test.inOrder();
+		} catch (Exception e) {
+			System.err.format("Exception occurred trying to read '%s'.",
+					filename);
+			e.printStackTrace();
+		}
+
 	}
 
 }
